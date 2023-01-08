7877c0d3af6fd9525889fe679e960d80ffd76ffb
adibo2 <Adibbensmina99@gmail.com>
2023-01-08 12:36:50 +0100

    add fixes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -74,7 +74,21 @@ import User from "../../../model/User";
 
 import CredentialsProvider from "next-auth/providers/credentials";
 export default NextAuth({
-
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user?._id) token._id = user._id;
+      // if (user?.isAdmin) token.isAdmin = user.isAdmin;
+      return token;
+    },
+    async session({ session, token }) {
+      if (token?._id) session.user._id = token._id;
+      // if (token?.isAdmin) session.user.isAdmin = token.isAdmin;
+      return session;
+    },
+  },
   providers: [
     CredentialsProvider({
       async authorize(credentials) {
@@ -96,22 +110,4 @@ export default NextAuth({
       },
     }),
   ],
-    session: {
-    strategy: "jwt",
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user?._id) token._id = user._id;
-      // if (user?.isAdmin) token.isAdmin = user.isAdmin;
-      // return token;
-      return Promise.resolve(token);
-    },
-    async session({ session, token }) {
-      if (token?._id) session.user._id = token._id;
-      // if (token?.isAdmin) session.user.isAdmin = token.isAdmin;
-      // return session;
-      return Promise.resolve(session);
-    },
-  },
-
 });
