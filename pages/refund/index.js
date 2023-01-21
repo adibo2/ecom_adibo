6daa5f86e30ec49e9fc3ba10ef7776c5a025edd1
adibo2 <Adibbensmina99@gmail.com>
2023-01-21 23:57:58 +0100

    add Mainpage

--- pages/refund/index.js
+++ pages/refund/index.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import css from 'styled-jsx/css'
 import Filter from '../../components/Filter/Filter'
 import Footer from '../../components/Footer/Footer'
 import Navbar from '../../components/Navbar/Navbar'
 import Refundx from '../../footc/Refund'
+import styles from "./../../styles/Home.module.scss"
 
 const Refund = () => {
   return (
-    <>
+    <div >
     <Navbar></Navbar>
     <Filter></Filter>
     <Refundx></Refundx>
     <Footer></Footer>
-    </>
+    </div>
   )
 }
 
